fb66c1832f8b77e8181661c4a2506ac89d6c091a
Letov Sergey <letovs@inbox.ru>
2025-03-03 21:18:49 +0300

    added auth page

---
+++ src/app/pages/auth/auth.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth',
+  imports: [],
+  templateUrl: './auth.component.html',
+  standalone: true,
+  styleUrl: './auth.component.scss'
+})
+export class AuthComponent {
+
+}
