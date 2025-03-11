685e1c52027b9deeefc868421aea1de1908e15f7
Letov Sergey <letovs@inbox.ru>
2025-03-11 07:04:19 +0300

    added obs

---
+++ client/src/app/shared/api/index.ts
@@ -0,0 +1,8 @@
+import {environment} from '../../../environments/environment.development';
+
+const baseUrl: string =  environment.apiUrl;
+
+export const API= {
+  auth: baseUrl,
+  registration: baseUrl
+}
