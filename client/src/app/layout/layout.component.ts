776362c93fda004ba4a77792c6ed42b779c102aa
Letov Sergey <letovs87@gmail.com>
2025-03-14 07:44:27 +0300

    move layout up dir

--- client/src/app/layout/layout.component.ts
+++ client/src/app/layout/layout.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-layout',
   imports: [],
+  standalone: true,
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
