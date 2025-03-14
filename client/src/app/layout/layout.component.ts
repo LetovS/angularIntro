56079e61b830832e74fb2373fbae6673960dfc3f
Letov Sergey <letovs87@gmail.com>
2025-03-14 08:22:22 +0300

    added styling

--- client/src/app/layout/layout.component.ts
+++ client/src/app/layout/layout.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
-import {HeaderComponent} from './header/header/header.component';
-import {FooterComponent} from './footer/footer/footer.component';
-import {SideComponent} from './side/side/side.component';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {AsideComponent} from './side/aside.component';
+import {RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [HeaderComponent, FooterComponent, SideComponent],
+  imports: [
+    RouterModule,
+    HeaderComponent,
+    FooterComponent,
+    AsideComponent],
   standalone: true,
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
