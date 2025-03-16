2dcda3b00fd2e6976a3258970060096ce4586c87
Letov Sergey <letovs87@gmail.com>
2025-03-16 18:28:31 +0300

    added datetime pipe

--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [DatePipe],
   standalone: true,
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+  dateTime: Date;
+  constructor() {
+  }
+    ngOnDestroy(): void {
+        throw new Error('Method not implemented.');
+    }
+    ngOnInit(): void {
+        setInterval(() => {
+          this.dateTime = new Date();
+        }, 1000)
+    }
 
 }
