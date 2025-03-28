dcab5ff3bc263ed3a6bfa57f1bdebfa79aec3cf5
Letov Sergey <letovs87@gmail.com>
2025-03-28 12:15:56 +0300

    add modal to change password blank

--- client/src/app/pages/settings/settings.component.ts
+++ client/src/app/pages/settings/settings.component.ts
@@ -3,17 +3,30 @@ import {SettingItemComponent} from './setting-item/setting-item.component';
 import {RouterLink} from '@angular/router';
 import {Button} from 'primeng/button';
 import {ChangePasswordComponent} from './change-password/change-password.component';
+import {Card} from 'primeng/card';
+import {ModalComponent} from '../../common/modal/modal.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {InputText} from 'primeng/inputtext';
+import {ITour} from '../../models/tour/tour';
 
 @Component({
   selector: 'app-settings',
-  imports: [SettingItemComponent, ChangePasswordComponent, RouterLink, Button],
+  imports: [SettingItemComponent, ChangePasswordComponent, RouterLink, Button, Card, ModalComponent, FormsModule, InputText, ReactiveFormsModule],
   templateUrl: './settings.component.html',
   standalone: true,
   styleUrl: './settings.component.scss'
 })
 export class SettingsComponent {
+  isModalOpen: boolean = false;
   menuItems = [
     { label: 'Смена пароля', path: 'change-password' },
     { label: 'Статистика', path: 'statistics' }
   ];
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
+  openModal() {
+    this.isModalOpen = true;
+  }
 }
